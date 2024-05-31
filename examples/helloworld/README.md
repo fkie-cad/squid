4efedbbe04ce0763ed58273a393d6d5b157d468c
Patrick D. <77979557+pd-fkie@users.noreply.github.com>
2024-05-31 23:48:10 +0200

    Added README to helloworld example

---
+++ examples/helloworld/README.md
@@ -0,0 +1,22 @@
+# Running a hello world program with squid
+
+This example showcases the bare minimum steps necessary to get `squid` up and running.
+
+# Build the target
+Enter the `squid-toolchain` container:
+```
+docker run --rm -it -v "$PWD:/io" squid-toolchain
+```
+
+And execute
+```
+pacman -S make
+cd /io
+make
+```
+
+## Run the emulator
+Simply run
+```
+cargo run --example helloworld
+```
--- examples/helloworld/run-helloworld.rs
+++ examples/helloworld/run-helloworld.rs
@@ -9,6 +9,7 @@ use squid::{
     passes::ImageDOTPass,
 };
 
+// Do one run of the target binary from its entrypoint to exit()
 fn execute(mut runtime: MultiverseRuntime) -> Result<(), MultiverseRuntimeFault> {
     loop {
         match runtime.run()? {
@@ -17,34 +18,35 @@ fn execute(mut runtime: MultiverseRuntime) -> Result<(), MultiverseRuntimeFault>
                 
                 match number {
                     syscalls::write => {
+                        // Get syscall arguments
                         let buf = runtime.get_gp_register(GpRegister::a1) as VAddr;
                         let len = runtime.get_gp_register(GpRegister::a2) as usize;
                         
                         let data = runtime.load_slice(buf, len)?;
                         let data = std::str::from_utf8(data).unwrap();
                         
+                        // Do syscall action
                         print!("{}", data);
                         
+                        // Set syscall return value
                         runtime.set_gp_register(GpRegister::a0, len as u64);
                     },
                     syscalls::exit_group => {
                         let code = runtime.get_gp_register(GpRegister::a0) as i8;
-                        println!("Exited with code: {}", code);
-                        break;
+                        std::process::exit(code as i32);
                     },
-                    _ => unreachable!("Syscall {} not handled", number),
+                    _ => unreachable!(),
                 }
             },
-            event => unreachable!("Event {} not handled", event),
+            _ => unreachable!(),
         }
     }
-    
-    Ok(())
 }
 
 fn main() {
     let mut compiler = Compiler::load_elf("./helloworld", &[], &[]).unwrap();
     
+    // this is of course optional
     compiler.run_pass(&mut ImageDOTPass::new("process_image.dot")).unwrap();
     
     let backend = MultiverseBackend::builder()
