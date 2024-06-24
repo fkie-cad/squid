017a891f3b05899d8a93ce34639a184a57fba92d
Patrick D. <77979557+pd-fkie@users.noreply.github.com>
2024-06-24 23:53:33 +0200

    Updated documentation index

--- squid/src/lib.rs
+++ squid/src/lib.rs
@@ -6,11 +6,21 @@
 //! During runtime, you get full control over your target by handling all system calls and other events yourself.
 //! This makes it easy to create and combine new sanitizers and test programs for all kinds of vulnerabilities, not just memory corruptions.
 //!
-//! # Examples, wiki and more...
+//! # Where to start
+//! Everything in `squid` starts with the [`Compiler`], so have a look at that.
+//! 
+//! # Examples, a wiki and more...
 //! ...can be found in the [repository](https://github.com/fkie-cad/squid).
+//! 
+//! # What is supported
+//! Binaries compiled with `squid`s own RISC-V toolchain and this special set of flags:
+//! ```
+//! -fPIE -pie -O0 -g -fno-jump-tables -mno-relax -D__thread=
+//! ```
 //! 
 //! # Features
 //! - `tui` (enabled by default): Enables animations and fancy loading graphics
+//! 
 
 #![feature(btree_extract_if)]
 
